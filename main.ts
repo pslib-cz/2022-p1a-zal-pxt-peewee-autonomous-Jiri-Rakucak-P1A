2c3856ada28adb63b89855f90993b551e092275d
Jiri-Rakucak-P1A <115144255+Jiri-Rakucak-P1A@users.noreply.github.com>
2023-06-21 12:35:15 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,13 @@
-// nastavení základních parametrů
+let around = false
+let connected = 0
+let whiteline = 0
 radio.setGroup(1)
-
 let speedFactor = 50
 let pin_M = DigitalPin.P15
 let pin_R = DigitalPin.P14
 let pin_L = DigitalPin.P13
 let pin_Trig = DigitalPin.P2
 let pin_Echo = DigitalPin.P1
-let whiteline = 0
-let connected = 0
 pins.setPull(pin_L, PinPullMode.PullNone)
 pins.setPull(pin_R, PinPullMode.PullNone)
 pins.setPull(pin_M, PinPullMode.PullNone)
@@ -16,108 +15,76 @@ let where = 0
 let manual = false
 let line_follower = true
 let speed = 160
-let around = false
-//  Funkce na zlehčení použití motorů
+let speedL = 155
+let speedR = 155
 function motor_run(left: number = 0, right: number = 0) {
-    PCAmotor.MotorRun(PCAmotor.Motors.M1, Math.map(Math.constrain(-1 * left * (speedFactor / 100), -100, 100), -100, 100, -200, 200))
-    PCAmotor.MotorRun(PCAmotor.Motors.M4, Math.map(Math.constrain(-1 * right * (speedFactor / 100), -100, 100), -100, 100, -255, 230))
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, Math.map(Math.constrain(-1 * left * (speedFactor / 100), -100, 100), -100, 100, -speedL, speedL))
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, Math.map(Math.constrain(-1 * right * (speedFactor / 100), -100, 100), -100, 100, -speedR, speedR))
 }
 
 
 radio.onReceivedString(function(receivedString: string) {
     if (receivedString === "forward") { 
         where = 1
-        basic.showLeds(`
-    . . # . .
-    . # # # .
-    # . # . #
-    . . # . .
-    . . # . .
-    `)}
+    }
+    if (receivedString == "left") {
+        where = 0}
     
-    if (receivedString === "left") { 
-        where = 0
-        basic.showLeds(`
-    . . # . .
-    . # . . .
-    # # # # #
-    . # . . .
-    . . # . .
-    `) }
-    if (receivedString === "right") { 
+    if (receivedString == "right") {
         where = 2
-        basic.showLeds(`
-    . . # . .
-    . . . # .
-    # # # # #
-    . . . # .
-    . . # . .
-    `)
-    } }
-)
+    }
+})
 
-basic.forever(function on_forever() {
-
+basic.forever(function () {
     let obstacle_distance = sonar.ping(pin_Trig, pin_Echo, PingUnit.Centimeters, 100)
-    let l = (whiteline ^ pins.digitalReadPin(pin_L)) == 0 ? false : true
-    let r = (whiteline ^ pins.digitalReadPin(pin_R)) == 0 ? false : true
-    let m = (whiteline ^ pins.digitalReadPin(pin_M)) == 0 ? false : true
-
-    if (line_follower) {
+let l = (whiteline ^ pins.digitalReadPin(pin_L)) == 0 ? false : true
+let r = (whiteline ^ pins.digitalReadPin(pin_R)) == 0 ? false : true
+let m = (whiteline ^ pins.digitalReadPin(pin_M)) == 0 ? false : true
+if (line_follower) {
         // jízda na senzory
         if (obstacle_distance <= 15 && obstacle_distance != 0) {
-            //  když sonar zaznamená méně než 15 cm
+            // když sonar zaznamená méně než 15 cantimetru, otoč servo a znovu změří
             PCAmotor.Servospeed(PCAmotor.Servos.S1, 0, 90, 1)
-            basic.pause(100)
-            if (obstacle_distance <= 15 && obstacle_distance != 0){
-                //  => objet objekt 20x20x20 ...
+            basic.pause(1000)
+            if (obstacle_distance <= 15 && obstacle_distance != 0) {
                 motor_run(199, -130)
                 basic.pause(50)
-                // doprava
+
                 motor_run(199, 130)
                 basic.pause(150)
-                // rovně
+
                 motor_run(-199, 130)
                 basic.pause(100)
-                // doleva
+
                 motor_run(199, 130)
                 basic.pause(750)
-                // rovně
+
                 motor_run(-199, 130)
                 basic.pause(100)
-                // doleva
+
                 motor_run(199, 130)
                 basic.pause(380)
-                // rovně
-                motor_run(199, 130)
-                basic.pause(100)
-            }
-            else{
-                motor_run(-200, 130)
+            } else {
+                motor_run(-150, 90)
                 basic.pause(50)
-                // doprava
-                motor_run(200, 130)
+
+                motor_run(150, 90)
                 basic.pause(150)
-                // rovně
-                motor_run(200, -130)
+
+                motor_run(150, -90)
                 basic.pause(100)
-                // doleva
-                motor_run(200, 130)
+
+                motor_run(150, 90)
                 basic.pause(750)
-                // rovně
-                motor_run(200, -130)
+
+                motor_run(150, -90)
                 basic.pause(100)
-                // doleva
-                motor_run(200, 130)
+
+                motor_run(150, 90)
                 basic.pause(380)
-                // rovně
-                motor_run(200, 130)
-                basic.pause(100)
-
             }
-
-            // doleva
             around = false
+           
         } else if (!m && !l && !r) {
             // couvání
             motor_run(-200 -160)
@@ -141,14 +108,14 @@ basic.forever(function on_forever() {
         } else if (!r && !l && m) {
             // když snímá jenom prostřední senzor => jeď rovně
             motor_run(200, 230)
+
         } else if (l && m) {
-            //  když levý a prostřední senzor snímá čáru => zaboč doleva
-            motor_run(100, 0)
+            // zatoč doleva
+            motor_run(speedL, 0 )
+
         } else if (r && m) {
-            //  když pravý a prostřední senzor snímá čáru => zaboč doprava
-            motor_run(0, 100)
+            // zatoč doprava
+            motor_run(0, speedR)
         }
-
     }
-
 })
